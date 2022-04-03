3f1a073b060f66879b2e8879a85c06fa04ba4a5a
Klaudia <102894779+KlaudiaGr@users.noreply.github.com>
2022-04-03 11:29:49 +0200

    Checking the repeatability of numbers

--- graliczbowa.py
+++ graliczbowa.py
@@ -14,12 +14,32 @@ liczba6 = random.randint(1,49)
 
 print("Podaj 6 liczb z zakresu od 1 do 49")
 l1uzytkownik = int(input("Liczba 1: "))
+while (l1uzytkownik > 49) or (l1uzytkownik < 1):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l1uzytkownik = int(input("liczba 1: "))
 l2uzytkownik = int(input("Liczba 2:"))
+while (l2uzytkownik > 49) or (l2uzytkownik < 1) or (l2uzytkownik == l1uzytkownik):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l2uzytkownik = int(input("liczba 2: "))
 l3uzytkownik = int(input("Liczba 3:"))
+while (l3uzytkownik > 49) or (l3uzytkownik < 1) or (l3uzytkownik == l2uzytkownik) or (l3uzytkownik == l1uzytkownik):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l3uzytkownik = int(input("liczba 3: "))
 l4uzytkownik = int(input("Liczba 4:"))
+while (l4uzytkownik > 49) or (l4uzytkownik < 1) or (l4uzytkownik == l2uzytkownik) or (l4uzytkownik == l1uzytkownik) \
+        or (l4uzytkownik == l3uzytkownik):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l4uzytkownik = int(input("liczba 4: "))
 l5uzytkownik = int(input("Liczba 5:"))
+while (l5uzytkownik > 49) or (l5uzytkownik < 1) or (l5uzytkownik == l2uzytkownik) or (l5uzytkownik == l1uzytkownik) \
+        or (l5uzytkownik == l3uzytkownik) or (l5uzytkownik == l4uzytkownik):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l5uzytkownik = int(input("liczba 5: "))
 l6uzytkownik = int(input("Liczba 6:"))
-
+while (l6uzytkownik > 49) or (l6uzytkownik < 1) or (l6uzytkownik == l2uzytkownik) or (l6uzytkownik == l1uzytkownik) \
+        or (l6uzytkownik == l3uzytkownik) or (l6uzytkownik == l4uzytkownik) or(l6uzytkownik == l5uzytkownik):
+    print("Błędna liczba, podaj nową z zakresu od 1 do 49")
+    l6uzytkownik = int(input("liczba 6: "))
 
 print("Wylosowane liczby to: ", liczba1, liczba2, liczba3, liczba4, liczba5, liczba6)
 print("Twoje liczby to: ", l1uzytkownik, l2uzytkownik, l3uzytkownik, l4uzytkownik, l5uzytkownik, l6uzytkownik)
